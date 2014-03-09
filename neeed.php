682f8eed27920e6fc0b9ff163e124e37811680a8
56k <56k@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-09 15:41:58 +0000

    Version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/neeed/tags/1.1@872252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- neeed.php
+++ neeed.php
@@ -49,7 +49,10 @@ function display_my_neeed($title = '' , $h2_class = '' , $ul_class = '' , $li_cl
  	}
  	else{
  		
- 		$url 		= 'http://neeed.com/api/get.php?key='. trim($neeed_api_key) .'&mode=articles&count='.$neeed_art_display;
+ 		
+ 		$bloginfo 	= get_bloginfo( 'url' );
+ 		
+ 		$url 		= 'http://neeed.com/api/get.php?key='. trim($neeed_api_key) .'&author_url='.$bloginfo.'&mode=articles&count='.$neeed_art_display;
 	 	$content 	= file_get_contents($url);
 	 	$articles 	= json_decode($content, true);
  	
@@ -128,9 +131,11 @@ function get_neeed_articles($atts){
 	
 		$neeed_user 	= $atts['user'];
 		$neeed_list 	= $atts['list'];
-			
 		
-		$url 		= 'http://neeed.com/api/get.php?key='. $neeed_api_key .'&mode=articles&count='. $neeed_limit .'&user='. $neeed_user .'&list='. $neeed_list .'  ';
+		$bloginfo 	= get_bloginfo( 'url' );	
+		
+		$url 		=  'http://neeed.com/api/get.php?key='. $neeed_api_key .'&author_url='. $bloginfo .'&mode=articles&count='. $neeed_limit .'&user='. $neeed_user .'&list='. $neeed_list ;
+				
 		$content 	= file_get_contents($url); 
 		
 			 		 	
@@ -293,7 +298,10 @@ class neeedwidget extends WP_Widget{
 			$title		= trim($title); 
 			
 			
-			$url 		= 'http://neeed.com/api/get.php?key='. $neeed_api_key .'&mode=articles&count='. $limit;
+			$bloginfo 	= get_bloginfo( 'url' );	
+		
+			
+			$url 		= 'http://neeed.com/api/get.php?key='. $neeed_api_key .'&author_url='.$bloginfo.'&mode=articles&count='. $limit;
 			$content	= file_get_contents($url); 
 			$articles 	= json_decode($content, true);
 			
